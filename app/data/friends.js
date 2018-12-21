dcdab802c930ac372ca6c9707c44aba7eaa436f9
Lamuel27 <samrelliner@gmail.com>
2018-12-21 16:44:18 -0600

    still trying to figure out how to push radio button values

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var list = [
+    {
+      name: 'Sam Rehlinger',
+      profPic: 'https://avatars1.githubusercontent.com/u/39862745?s=460&v=4',
+      scores: [
+        "3",
+        "5",
+        "5",
+        "4",
+        "2",
+        "5",
+        "4",
+        "4",
+        "5",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = list;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,35 @@
+var list = require("../data/friends");
+
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(list);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        var user = req.body;
+
+        for (var i = 0; i < user.scores.length; i++) {
+            user.scores[i] = parseInt(user.scores[i]);
+        }
+
+        var bestMatch = 0;
+        var minDifference = 30;
+
+        for (var i = 0; i < list.length; i++) {
+            var totalDifference = 0;
+            for (var j = 0; j < list[i].scores.length; j++) {
+                var difference = Math.abs(user.scores[j] - list[i].scores[j]);
+                totalDifference += difference;
+            }
+
+            if (totalDifference < minDifference) {
+                bestMatch = i;
+                minDifference = totalDif;
+            }
+        }
+
+        list.push(user);
+
+        res.json(list[bestMatch]);
+    });
+};
